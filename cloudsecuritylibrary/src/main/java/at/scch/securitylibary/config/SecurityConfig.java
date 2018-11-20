6710898fec859222ed5b4c3e5f444b848374d181
BenajminMayer <benjamin.mayer@jku.at>
2018-11-20 14:04:06 +0100

    Add cors support for web client

--- cloudsecuritylibrary/src/main/java/at/scch/securitylibary/config/SecurityConfig.java
+++ cloudsecuritylibrary/src/main/java/at/scch/securitylibary/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package at.scch.securitylibary.config;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableOAuth2Sso
@@ -13,7 +16,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
     protected void configure(HttpSecurity http) throws Exception {
 		http
-		.authorizeRequests().anyRequest().permitAll();
+    	.cors()
+    	.and().
+    	authorizeRequests().anyRequest().permitAll();
     }
+	
+	@Bean
+   	public WebMvcConfigurer corsConfigurer() {
+   		return new WebMvcConfigurer() {
+   			@Override
+   			public void addCorsMappings(CorsRegistry registry) {
+   				registry.addMapping("/**").allowedMethods("PUT", "DELETE", "GET", "OPTIONS", "POST", "PATCH");
+   			}
+   		};
+   	} 
 
 }
